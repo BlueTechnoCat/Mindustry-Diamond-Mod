50865f70525efaed8f62b4963d6f6c404aee8207
BlueTechno <malavervj@gmail.com>
2021-04-23 23:41:00 -0400

    Hmmm...

--- scripts/gss-shrapnel.js
+++ scripts/gss-shrapnel.js
@@ -14,16 +14,14 @@ var h = 0;
 
 greatShiningStar.buildType = () => extend(ItemTurret.ItemTurretBuild, {
 
-    collision(bullet) {
-        this.super$collision(bullet);
-        if (bullet.team != this.team && Math.random() * 100 <= 40) {
-            while (h < 4) {
-                shiningFrag.create(this, this.team, this.x, this.y, (bullet.rotation() + 180) + ((Math.random() - 0.5) * 8) * 2);
-                h++;
+        collision(bullet) {
+            this.super$collision(bullet);
+            if (bullet.team != this.team && Math.random() * 100 <= 40) {
+                while (h < 4) {
+                    shiningFrag.create(this, this.team, this.x, this.y, (bullet.rotation() + 180) + ((Math.random() - 0.5) * 8) * 2);
+                    h++;
+                }
+                h = 0;
             }
-            h = 0;
         }
-    }
-
-
-});
+    });
