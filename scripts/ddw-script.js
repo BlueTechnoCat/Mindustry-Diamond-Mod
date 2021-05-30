4641c6037d3d808138cd51aa8c43e0eb65f76fd9
BlueTechnoCat <bluetechnocat@gmail.com>
2021-05-30 16:17:06 -0400

    Testing the New script

--- scripts/ddw-script.js
+++ scripts/ddw-script.js
@@ -1,17 +1,24 @@
-const healAmount = 25;
-const delay = 200;
+let healing = 25;
+let reload = 150;
+let range = 8;
 
-const darkDiamondWall = extend(Wall, "dark-diamond-wall", {});
+const darkDiamondWall = extend(ForceProjector, "dark-diamond-wall", {
 
-darkDiamondWall.buildType = () => extend(Wall.WallBuild, darkDiamondWall, {
+	setStats() {
+		this.super$setStats();
+		this.stats.add(Stat.repairTime, (int)(100 / healing * reload / 60), Stat.seconds);
+		this.stats.add(Stat.range, range / 8, StatUnit.blocks);
+	}
+});
+
+darkDiamondWall.buildType = () => extend(ForceProjector.ForceBuild, darkDiamondWall, {
 
     updateTile() {
-        if (this.damaged() && this.timer.get(0, delay)) {
-            this.heal(this.maxHealth / healAmount);
+        if (this.damaged() && this.timer.get(0, reload)) {
+            this.heal(this.maxHealth / healing);
             Fx.healBlockFull.at(this.x, this.y, 2, Pal.heal);
         }
     }
-
 });
 
 const darkDiamondWallLarge = extend(Wall, "dark-diamond-wall-large", {});
@@ -19,10 +26,9 @@ const darkDiamondWallLarge = extend(Wall, "dark-diamond-wall-large", {});
 darkDiamondWallLarge.buildType = () => extend(Wall.WallBuild, darkDiamondWallLarge, {
 
     updateTile() {
-        if (this.damaged() && this.timer.get(0, delay)) {
-            this.heal(this.maxHealth / healAmount);
+        if (this.damaged() && this.timer.get(0, 150)) {
+            this.heal(this.maxHealth / 40);
             Fx.healBlockFull.at(this.x, this.y, 2, Pal.heal);
         }
     }
-
 });
